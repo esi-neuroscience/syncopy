7bd193e8d9cb6c39e30979eee8cbba2916e751dd
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2019-03-01 13:04:21 +0100

    FIX: Corrected errorenous update of datatype example
    Changes to be committed:
    	modified:   ex_datatype.py

--- spykewave/examples/ex_datatype.py
+++ spykewave/examples/ex_datatype.py
@@ -1,8 +1,11 @@
-# ex_datatype.py - Example script illustrating usage of `BaseData` class
-# 
-# Created: Januar 17 2019
-# Last modified by: Stefan Fuertinger [stefan.fuertinger@esi-frankfurt.de]
-# Last modification time: <2019-01-30 13:46:45>
+# -*- coding: utf-8 -*-
+#
+# Example script illustrating usage of `AnalogData` class
+#
+# Created: 2019-02-25 13:08:56
+# Last modified by: Joscha Schmiedt [joscha.schmiedt@esi-frankfurt.de]
+# Last modification time: <2019-03-01 12:58:07>
+
 
 # Builtin/3rd party package imports
 import numpy as np
@@ -17,50 +20,65 @@ if spw_path not in sys.path:
 # Import Spykewave
 import spykewave as sw
 
-# Formal defintion of function for Sphinx docstring building
+
 def ex_datatype():
     """
     Docstring...
     """
 
+
+def time2sample(t, dt=0.001):
+    return np.asarray(t / dt, dtype=np.uint64)
+
+
+def sample2time(s, dt=0.001):
+    return s * dt
+
+
 # This prevents Sphinx from executing the script
 if __name__ == "__main__":
 
-    # # Set path to data directory
-    # datadir = ".." + os.sep + ".." + os.sep + ".." + os.sep + ".." + os.sep + "Data"\
-    #           + os.sep + "Joscha_testdata" + os.sep
-    # 
-    # # Choose data-set to read
-    # dataset = "klecks_20180406_rfmapping-bar_1_xWav.lfp"
-    # 
-    # # Create SpykeWave data object
-    # data = sw.BaseData(datadir + dataset)
-
-    # # ChunkData testing
-    # a = np.memmap('a.array', dtype='float64', mode='w+', shape=( 5000,1000)) # 38.1MB
-    # a[:,:] = 111
-    # b = np.memmap('b.array', dtype='float64', mode='w+', shape=(15000,1000)) # 114 MB
-    # b[:,:] = 222
-    # c = np.memmap('c.array', dtype='float64', mode='w+', shape=(10000,1000)) # 114 MB
-    # c[:,:] = 333
-    # d = ChunkData([a,b,c]) 
-
     # Set path to data directory
-    #datadir = ".." + os.sep + ".." + os.sep + ".." + os.sep + ".." + os.sep + "Data"\
-    #          + os.sep + "testdata" + os.sep
-    datadir = os.path.join(os.sep, "mnt", "hpx", "it", "dev", "SpykeWave", "testdata") + os.sep
+    # datadir = ".." + os.sep + ".." + os.sep + ".." + os.sep + ".." + os.sep + "Data"\
+    #           + os.sep + "testdata" + os.sep
+    datadir = os.path.join(os.sep, "mnt", "hpx", "it",
+                           "dev", "SpykeWave", "testdata")
+    basename = "MT_RFmapping_session-168a1"
 
-    files = ["MT_RFmapping_session-168a1_xWav.lfp", "MT_RFmapping_session-168a1_xWav.lfp"]
-    trl = np.array([[0, 20000, 2],
-                    [20000, 40000, 3],
-                    [40000, 150000, 1],
-                    [150000, 200000, 5],
-                    [200000, 350000, 4],
-                    [350000, 400000, 0]])
-    data = sw.BaseData(filename=[datadir + file for file in files],
-                       filetype="esi",
-                       trialdefinition=trl)
+    files = [basename + "_xWav.lfp",
+             basename + "_xWav.mua",
+             basename + "_Raws.lfp",
+             basename + "_Raws.mua"]
+    channelLabels = []
+    channelLabels.extend(["ecogLfp_{:03d}".format(idx) for idx in range(256)])
+    channelLabels.extend(["ecogMua_{:03d}".format(idx) for idx in range(256)])
+    channelLabels.extend(["vprobeLfp_{:03d}".format(idx) for idx in range(24)])
+    channelLabels.extend(["vprobeMua_{:03d}".format(idx) for idx in range(24)])
 
-    # # Choose data-set to read
-    # filename = "klecks_20180406_rfmapping-bar_1_xWav.lfp"
-    
+    data = sw.AnalogData(filename=[os.path.join(datadir, file) for file in files],
+                         filetype="esi")
+
+    # define trials from photodiode onsets
+    pdFile = os.path.join(datadir, basename + ".dpd")
+    pdData = np.memmap(pdFile,
+                       dtype=np.uint64,
+                       mode='r',
+                       offset=128).reshape((2, -1))
+    # photodiode was recorded with 1017.25... Hz
+    pdDt = 24 / 24414.0625
+
+    # Trials start 200 ms before stimulus onset
+    prestimTime = 0.2
+    tOnset = sample2time(pdData[0, pdData[1, :] == 1], dt=pdDt) - prestimTime
+    tOffset = sample2time(pdData[0, pdData[1, :] == 0], dt=pdDt) + 0.2
+    iOnset = time2sample(tOnset, dt=1 / data.samplerate)
+    iOffset = time2sample(tOffset, dt=1 / data.samplerate)
+    iZero = time2sample(prestimTime, dt=1 / data.samplerate)
+
+    trl = np.stack((iOnset, iOffset, np.ones(iOnset.shape, dtype=np.uint64) * iZero), axis=1)
+
+    trl = trl[trl[:, 1] - trl[:, 0] > 500, :].astype(int)
+    data = sw.AnalogData(filename=[os.path.join(datadir, file) for file in files],
+                         filetype="esi", trialdefinition=trl, channel=channelLabels)
+
+    data.save(os.path.join(datadir, 'testdata'))
