c494275501f3a2689f87657407bb26491cc71634
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2019-03-22 14:56:40 +0100

    NEW: Started writing test-routines for SyNCoPy I/O methods
    Changes to be committed:
    	new file:   syncopy/tests/test_spyio.py

---
+++ syncopy/tests/test_spyio.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# Test functionality of SyNCoPy-container I/O routines
+# 
+# Created: 2019-03-19 14:21:12
+# Last modified by: Stefan Fuertinger [stefan.fuertinger@esi-frankfurt.de]
+# Last modification time: <2019-03-22 14:54:46>
+
+class TestSpyIO(object):
+
+    # Allocate test-datasets for AnalogData, SpectralData, SpikeData and EventData objects
+    nc = 10
+    ns = 30
+    nt = 5
+    nf = 15
+    nd = 50
+    data = {}
+    trl = {}
+
+    # Generate 2D array simulating an AnalogData array
+    data["AnalogData"] = np.arange(1, nc*ns + 1).reshape(nc, ns)
+    trl["AnalogData"] = np.vstack([np.arange(0, ns, 5),
+                                   np.arange(5, ns + 5, 5),
+                                   np.ones((int(ns/5), )),
+                                   np.ones((int(ns/5), )) * np.pi]).T
+
+    # Generate a 4D array simulating a SpectralData array
+    data["SpectralData"] = np.arange(1, nc*ns*nt*nf + 1).reshape(ns, nt, nc, nf)
+    trl["SpectralData"] = trl["AnalogData"]
+
+    # Use a fixed random number generator seed to simulate a 2D SpikeData array
+    seed = np.random.RandomState(13)
+    data["SpikeData"] = np.vstack([seed.choice(ns, size=nd),
+                                   seed.choice(nc, size=nd),
+                                   seed.choice(int(nc/2), size=nd)]).T
+    trl["SpikeData"] = trl["AnalogData"]
+
+    # Define data classes to be used in tests below
+    classes = ["AnalogData", "SpectralData", "SpikeData"]
+
+    # Test correct handling of object log and cfg
+    def test_logging(self):
+        with tempfile.TemporaryDirectory() as tdir:
+            for dclass in self.classes:
+                dummy = getattr(swd, dclass)(self.data, trialdefinition=self.trl)
+                ldum = dummy._log
+                dummy.save("dummy")
+
+                # ensure saving is logged correctly
+                assert len(dummy.log) > ldum
+                assert dummy.cfg["method"] == "save_spy"
+
+    # CHECK CHECKSUM-MATCHING!
+    # CHECK FNAME
+    # CHECK APPEND_EXT
+    # CHECK MEMUSE
+    # CHECK FILESIZE
+    # CHEK STORAGE LOCATION (CORRECT FOLDER)
