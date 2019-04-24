43c6003f9fae406e7d8bf9010a96fe75f3e1033b
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2019-04-24 15:34:06 +0200

    CHG: Changed layout of custom tracebacks and added customization options
    - After a team-discussion a color-scheme and layout has been chosen for
      SyNCoPy's tracebacks (closes #28)
    - the new package-wide variable `__tbcount__` encodes the traceback depth
      shown in the prompt (set in package's __init__.py)
    - currently, only ``pdb`` and ``ipdb`` are supported with SyNCoPy's new
      custom exceptions
    - added Jupyter notebook temp directories to git's ignore list
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   syncopy/__init__.py
    	modified:   syncopy/utils/misc.py

--- syncopy/utils/misc.py
+++ syncopy/utils/misc.py
@@ -4,12 +4,15 @@
 # 
 # Created: 2019-01-14 10:23:44
 # Last modified by: Stefan Fuertinger [stefan.fuertinger@esi-frankfurt.de]
-# Last modification time: <2019-04-23 16:16:52>
+# Last modification time: <2019-04-24 15:16:56>
 
 # Builtin/3rd party package imports
 import sys
 import traceback
 from collections import OrderedDict
+
+# Local imports
+from syncopy import __tbcount__
 
 __all__ = ["SPYTypeError", "SPYValueError", "SPYIOError", "SPYExceptionHandler", "get_caller"]
 
@@ -98,36 +101,40 @@ class SPYIOError(Error):
                           else ": object does not exist" if self.exists is False else "")
 
     
-def SPYExceptionHandler(*excargs):
+def SPYExceptionHandler(*excargs, **exckwargs):
     """
     Docstring coming soon(ish)...
     """
-
-    # Max. depth for printing traceback information
-    max_count = 5
 
     # Depending on the number of input arguments, we're either in Jupyter/iPython
     # or "regular" Python - this matters for coloring error messages
     if len(excargs) == 3:
         isipy = False
+        nbpdb = False
         etype, evalue, etb = excargs
     else:
         isipy = True
+        nbpdb = False
+        ipy = get_ipython()
         etype, evalue, etb = sys.exc_info()
         cols = get_ipython().InteractiveTB.Colors
         cols.filename = cols.filenameEm
-        # self, etype, evalue, etb = excargs
+        cols.bold = "\033[1m"
+        if ipy.has_trait("kernel"):   # Monkey patch ``sys`` in case we're running in a notebook
+            sys.last_traceback = etb
+            if ipy.call_pdb:
+                nbpdb = True    # if %pdb was used in notebook, manually launch it at the end
 
     # Pass KeyboardInterrupt on to regular excepthook so that CTRL + C
     # can still be used to abort program execution (only relevant in "regular"
-    # Python prompts
+    # Python prompts)
     if issubclass(etype, KeyboardInterrupt) and not isipy:
         sys.__excepthook__(etype, evalue, etb)
         return
 
     # Starty by putting together first line of error message
     emsg = "{}\nSyNCoPy encountered an error in{} \n\n".format(cols.topline if isipy else "",
-                                                            cols.Normal if isipy else "")
+                                                               cols.Normal if isipy else "")
 
     # Build an ordered(!) dictionary that encodes separators for traceback components
     sep = OrderedDict({"filename": ", line ",
@@ -135,25 +142,23 @@ def SPYExceptionHandler(*excargs):
                        "name": "\n\t",
                        "line": "\n"})
 
-    # Go through traceback and put together a list of strings for printing
-    tb_list = []
-    tb_count = 0
-    for frame in traceback.extract_tb(etb):
-        if frame.filename.find("site-packages") < 0:
+    # Find "root" of traceback tree (and remove outer-most frames)
+    keepgoing = True
+    while keepgoing:
+        frame = traceback.extract_tb(etb)[0]
+        etb = etb.tb_next
+        if frame.filename.find("site-packages") < 0 or \
+           (frame.filename.find("site-packages") >= 0 and \
+            frame.filename.find("syncopy") >= 0):
             tb_entry = ""
             for attr in sep.keys():
                 tb_entry += "{}{}{}{}".format(getattr(cols, attr) if isipy else "",
                                               getattr(frame, attr),
                                               cols.Normal if isipy else "",
                                               sep.get(attr))
-            if tb_count == 0:
-                tb_entry += "\n" + "-"*30 + "\n"
-            tb_list.append(tb_entry)
-            tb_count += 1
-            if tb_count == max_count:
-                break
-    emsg += "".join(tb_list)
-
+            emsg += tb_entry
+            keepgoing = False
+    
     # Format the exception-part of the traceback - the resulting list usually
     # contains only a single string - if we find more (e.g., in case of a
     # SyntaxError), use everything
@@ -163,21 +168,51 @@ def SPYExceptionHandler(*excargs):
         idx = exc_msg.rfind(etype.__name__)
         if idx >= 0:
             exc_msg = exc_msg[idx + len(etype.__name__):]
+        exc_name = "{}{}{}{}".format(cols.excName if isipy else "",
+                                     cols.bold if isipy else "",
+                                     etype.__name__,
+                                     cols.Normal if isipy else "")
     else:
         exc_msg = "".join(exc_fmt)
+        exc_name = ""
 
-    # Highlight (if we're running in iPython) the name of the encountered exception
-    emsg += "\n{}{}{}".format(cols.excName if isipy else "",
-                              etype.__name__,
-                              cols.Normal if isipy else "")
+    # Glue actual Exception name + message to output string
+    emsg += "\n{}{}{}{}".format(exc_name,
+                                cols.bold if isipy else "",
+                                exc_msg,
+                                cols.Normal if isipy else "",)
 
-    # Finally, append the actual exception message and some general info
-    emsg += exc_msg
-    emsg += "\nUse `sys.last_traceback` to inspect the full error traceback"
+    # Now go through traceback and put together a list of strings for printing
+    if __tbcount__:
+        emsg += "\n" + "-"*30 + "\nAbbreviated traceback:\n\n"
+        tb_count = 0
+        tb_list = []
+        for frame in traceback.extract_tb(etb):
+            if frame.filename.find("site-packages") < 0 or \
+               (frame.filename.find("site-packages") >= 0 and \
+                frame.filename.find("syncopy") >= 0):
+                tb_entry = ""
+                for attr in sep.keys():
+                    tb_entry += "{}{}{}{}".format("", # placeholder for color if wanted
+                                                  getattr(frame, attr),
+                                                  "", # placeholder for color if wanted
+                                                  sep.get(attr))
+                tb_list.append(tb_entry)
+                tb_count += 1
+                if tb_count == __tbcount__:
+                    break
+        emsg += "".join(tb_list)
+
+    # Finally, another info message
+    emsg += "\nUse `import traceback; traceback.print_tb(sys.last_traceback)` " + \
+            "to inspect the full error traceback"
 
     # Show generated message and get outta here
     print(emsg)
 
+    # Kick-start debugging in case we're running in a notebook with %pdb enabled
+    if nbpdb:
+        ipy.InteractiveTB.debugger()
     
 def get_caller():
     """
