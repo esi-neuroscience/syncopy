3558da6c5b2595c1f6829a42ead6087861ffe583
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2019-04-29 16:45:23 +0200

    FIX: Do not assume iPython is always run interactively
    - in case iPython is used to run a script, SyNCoPy's custom exception
      raised an exception itself due to the invocation of ``get_ipython()``
      in the beginning. This has been fixed (closes #80)
    
    Changes to be committed:
    	modified:   syncopy/examples/ex_simple.py
    	modified:   syncopy/utils/misc.py

--- syncopy/examples/ex_simple.py
+++ syncopy/examples/ex_simple.py
@@ -4,7 +4,7 @@
 # 
 # Created: 2019-03-28 16:48:31
 # Last modified by: Stefan Fuertinger [stefan.fuertinger@esi-frankfurt.de]
-# Last modification time: <2019-04-25 17:05:43>
+# Last modification time: <2019-04-29 16:39:57>
 
 # Builtin/3rd party package imports
 import h5py
@@ -28,7 +28,7 @@ basename = "MT_RFmapping_session-168a1"
 
 # Create `AnalogData` object from *.lfp/*.mua files
 adataFiles = [os.path.join(datadir, basename + ext) for ext in ["_xWav.lfp", "_xWav.mua"]]
-adata = spy.AnalogData(filename=adataFiles, filetype="esi")
+adata = spy.AnalogData(filename=adataFiles, filetype="esis")
 
 sys.exit()
 
--- syncopy/utils/misc.py
+++ syncopy/utils/misc.py
@@ -4,7 +4,7 @@
 # 
 # Created: 2019-01-14 10:23:44
 # Last modified by: Stefan Fuertinger [stefan.fuertinger@esi-frankfurt.de]
-# Last modification time: <2019-04-24 15:16:56>
+# Last modification time: <2019-04-29 16:42:40>
 
 # Builtin/3rd party package imports
 import sys
@@ -113,17 +113,20 @@ def SPYExceptionHandler(*excargs, **exckwargs):
         nbpdb = False
         etype, evalue, etb = excargs
     else:
-        isipy = True
-        nbpdb = False
-        ipy = get_ipython()
         etype, evalue, etb = sys.exc_info()
-        cols = get_ipython().InteractiveTB.Colors
-        cols.filename = cols.filenameEm
-        cols.bold = "\033[1m"
-        if ipy.has_trait("kernel"):   # Monkey patch ``sys`` in case we're running in a notebook
-            sys.last_traceback = etb
-            if ipy.call_pdb:
-                nbpdb = True    # if %pdb was used in notebook, manually launch it at the end
+        try:                            # careful: if ipython is used to launch a script, ``get_ipython`` is not defined
+            ipy = get_ipython()
+            isipy = True
+            cols = get_ipython().InteractiveTB.Colors
+            cols.filename = cols.filenameEm
+            cols.bold = "\033[1m"
+            if ipy.has_trait("kernel"): # Monkey patch ``sys`` in case we're running in a notebook
+                sys.last_traceback = etb
+                if ipy.call_pdb:
+                    nbpdb = True        # if %pdb was used in notebook, manually launch it at the end
+        except:
+            isipy = False
+            nbpdb = False
 
     # Pass KeyboardInterrupt on to regular excepthook so that CTRL + C
     # can still be used to abort program execution (only relevant in "regular"
