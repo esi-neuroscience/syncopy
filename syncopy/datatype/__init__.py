eafcbc14b50426e9dd3d14ead374202ab927c6bf
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2020-01-24 13:42:31 +0100

    WIP: A new class for storing statistical data
    Currently only `TimelockData` for storing the output of `timelockanalysis`.
    Documentation and tests (and bugfixes) are still missing.

--- syncopy/datatype/__init__.py
+++ syncopy/datatype/__init__.py
@@ -4,13 +4,14 @@
 # 
 # Created: 2019-01-15 10:03:44
 # Last modified by: Joscha Schmiedt [joscha.schmiedt@esi-frankfurt.de]
-# Last modification time: <2020-01-24 09:25:34>
+# Last modification time: <2020-01-24 12:34:17>
 
 # Import __all__ routines from local modules
-from . import base_data, continuous_data, discrete_data, methods
+from . import base_data, continuous_data, discrete_data, methods, statistical_data
 from .base_data import *
 from .continuous_data import *
 from .discrete_data import *
+from .statistical_data import *
 from .methods.definetrial import *
 from .methods.padding import *
 from .methods.selectdata import *
@@ -20,6 +21,7 @@ __all__ = []
 __all__.extend(base_data.__all__)
 __all__.extend(continuous_data.__all__)
 __all__.extend(discrete_data.__all__)
+__all__.extend(statistical_data.__all__)
 __all__.extend(methods.definetrial.__all__)
 __all__.extend(methods.padding.__all__)
 __all__.extend(methods.selectdata.__all__)
