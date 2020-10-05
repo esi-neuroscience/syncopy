218b7e8656dc9f25b651551ea51cb8a573fd3886
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2020-10-05 20:04:17 +0200

    CHG: Modified pip-package deployment config
    - updated setup.py to include necessary/optional packages as processed by
      `conda2pip`
    - amended setup.cfg with `project_urls`, `home-page` and `classifier`s
    - updated .travis.yml to work w/new `conda2pip` dependency resolution script
    - included .eggs/ directory in .gitignore
    - simplified build command in .gitlab-ci.yml to be in one line
      (`python setup.py sdist bdist_wheel`)
    - removed requirements.txt and requirements-test.txt from repository: from now
      on both files are auto-generated on demand by `conda2pip` to be in sync
      with syncopy.yml
    
    On branch fixes_cleanup
    Changes to be committed:
    	modified:   .gitignore
    	modified:   .gitlab-ci.yml
    	modified:   .travis.yml
    	deleted:    requirements-test.txt
    	deleted:    requirements.txt
    	modified:   setup.cfg
    	modified:   setup.py
    	modified:   syncopy.yml

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-import setuptools
+from setuptools import setup
+from conda2pip import conda2pip
 
-setuptools.setup(
+# Get necessary and optional package dependencies
+required, dev = conda2pip(return_lists=True)
+
+# Run setup (note: identical arguments supplied in setup.cfg will take precedence)
+setup(
     setup_requires=['pbr'],
+    install_requires=required,
+    extras_require={"dev": dev},
     pbr=True
 )
-
