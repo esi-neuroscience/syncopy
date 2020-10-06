9e41da823e3988c1575227796ea788580e10ba7d
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2020-10-06 19:50:38 +0200

    CHG: Retire pbr for packaging
    - newer versions of pbr (>=4.5) suffer from multiple bugs that make
      PyPI deployment tedious (`python_requires` is not resolved correctly,
      `description-file` is not mapped onto `long_description`, extra
      requirements are not processed etc.); current versions of `setuptools`
      (>= 40) offer much of the same functionality (specifically support
      for static setup.cfg files) with much higher reliability. Thus, the
      build system has been re-tooled to exclusively use `setuptools` instead
      of `pbr`.
    
    On branch fixes_cleanup
    Changes to be committed:
    	modified:   .gitlab-ci.yml
    	modified:   setup.cfg
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+# Builtin/3rd party package imports
 from setuptools import setup
+
+# Local imports
 from conda2pip import conda2pip
 
 # Get necessary and optional package dependencies
@@ -6,8 +9,6 @@ required, dev = conda2pip(return_lists=True)
 
 # Run setup (note: identical arguments supplied in setup.cfg will take precedence)
 setup(
-    setup_requires=['pbr'],
     install_requires=required,
     extras_require={"dev": dev},
-    pbr=True
 )
