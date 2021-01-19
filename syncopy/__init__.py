d0979b6d16867652b3570ff4f095fe6046c982f3
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2021-01-19 10:52:53 +0100

    FIX: More GitLab CI fixes
    - corrected URL of PyPI-Test in GitLab CI setup
    - set environment variable `GIT_FETCH_EXTRA_FLAGS: --tags` to pull git tags
      from remote, so that package version is inferred correctly
    - do not use `__name__` in package __init__.py to get version from
      site-packages; SyNCoPy's PyPI name is "esi-syncopy", thus, querying for
      "syncopy" in site-packages does not yield anything
    
    On branch master
    Changes to be committed:
    	modified:   .gitlab-ci.yml
    	modified:   CITATION.cff
    	modified:   syncopy/__init__.py

--- syncopy/__init__.py
+++ syncopy/__init__.py
@@ -13,14 +13,14 @@ from importlib.metadata import version, PackageNotFoundError
 
 # Get package version: either via meta-information from egg or via latest git commit
 try:
-    __version__ = version(__name__)
+    __version__ = version("esi-syncopy")
 except PackageNotFoundError:
     proc = subprocess.Popen("git describe --always",
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             text=True, shell=True)
     out, err = proc.communicate()
     if proc.returncode != 0:
-        proc = subprocess.Popen("git rev-parse HEAD:acme/__init__.py",
+        proc = subprocess.Popen("git rev-parse HEAD:syncopy/__init__.py",
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 text=True, shell=True)
         out, err = proc.communicate()
