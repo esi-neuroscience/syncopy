eb03773e60109e75d72602f623edd2d9303a95ee
tensionhead <gregor.moenke@embl.de>
2021-08-04 14:51:30 +0200

    merge resolve

--- test_backend.py
+++ test_backend.py
@@ -4,13 +4,15 @@ import numpy as np
 import matplotlib.pyplot as ppl
 
 from syncopy.shared.parsers import data_parser, scalar_parser, array_parser 
-from syncopy.specest.wavelet import _get_optimal_wavelet_scales, wavelet
+from syncopy.specest.wavelet import get_optimal_wavelet_scales, wavelet
 from syncopy.specest.superlet import SuperletTransform, MorletSL, cwtSL, _get_superlet_support, superlet, compute_adaptive_order, scale_from_period
 from syncopy.specest.wavelets import Morlet
 from scipy.signal import fftconvolve
 
 
-def gen_superlet_testdata(freqs=[20, 40, 60], cycles=11, fs=1000):
+def gen_superlet_testdata(freqs=[20, 40, 60],
+                          cycles=11, fs=1000,
+                          eps = 0):
 
     '''
     Harmonic superposition of multiple
@@ -39,20 +41,24 @@ def gen_superlet_testdata(freqs=[20, 40, 60], cycles=11, fs=1000):
     # stack the packets together with some padding        
     signal.append(pad)
     signal = np.concatenate(signal)
+
+    # additive white noise
+    if eps > 0:
+        signal = np.random.randn(len(signal)) * eps + signal
     
     return signal
                        
 
 # test the Wavelet transform
 fs = 1000
-s1 = 1 * gen_superlet_testdata(fs=fs) # 20Hz, 40Hz and 60Hz
-data = np.c_[s1, 5*s1]
+s1 = 1 * gen_superlet_testdata(fs=fs, eps=0) # 20Hz, 40Hz and 60Hz
+data = np.c_[3*s1, 50*s1]
 preselect = np.ones(len(s1), dtype=bool)
 preselect2 = np.ones((len(s1), 2), dtype=bool)
 pads = 0
 
 ts = np.arange(-50,50)
-morletTC = Morlet(w0=5)
+morletTC = Morlet()
 morletSL = MorletSL(c_i=30)
 
 # frequencies to look at, 10th freq is around 20Hz
@@ -61,37 +67,54 @@ scalesTC = morletTC.scale_from_period(1 / freqs)
 # scales are cycle independent!
 scalesSL = scale_from_period(1 / freqs)
 
+# automatic diadic scales
+ssTC = get_optimal_wavelet_scales(Morlet().scale_from_period, len(s1), 1/fs)
+ssSL = get_optimal_wavelet_scales(scale_from_period, len(s1), 1/fs)
 # a multiplicative Superlet - a set of Morlets, order 1 - 30
 c_1 = 1
 cycles = c_1 * np.arange(1, 31)
 sl = [MorletSL(c) for c in cycles]
 
-# res = wavelet(data,
-#               preselect,
-#               preselect,
-#               pads,
-#               pads,
-#               samplerate=fs,
-#               # toi='some',
-#               output_fmt="pow",
-#               scales=scalesSL,
-#               wav=MorletSL(4),
-#               noCompute=True)
+res = wavelet(data,
+              preselect,
+              preselect,
+              pads,
+              pads,
+              samplerate=fs,
+              # toi='some',
+              output_fmt="pow",
+              scales=scalesTC,
+              wav=Morlet(),
+              noCompute=False)
 
 
-# spec = superlet(data, samplerate=fs, scales=scalesSL, order_max=30)
-nc = superlet(data, samplerate=fs, scales=scalesSL, order_max=30,noCompute=True)
+# unit impulse
+# data = np.zeros(500)
+# data[248:252] = 1
+spec = superlet(s1, samplerate=fs, scales=scalesSL,
+                order_max=10,
+                order_min=5,
+                adaptive=False)
+spec2 = superlet(data, samplerate=fs, scales=scalesSL, order_max=20, adaptive=False)
+
+# nc = superlet(data, samplerate=fs, scales=scalesSL, order_max=30)
 
 
 def do_slt(data, scales=scalesSL, **slkwargs):
+
+    if scales is None: 
+        scales = get_optimal_wavelet_scales(scale_from_period,
+                                            len(data[:, 0]),
+                                            1 / fs)    
+
     spec = superlet(data, samplerate=fs,
                     scales=scales,
                     **slkwargs)
 
-    print(spec.max())
+    print(spec.max(),spec.shape)
     ppl.figure()
     extent = [0, len(s1) / fs, freqs[-1], freqs[0]]    
-    ppl.imshow(np.abs(spec[:,0,:,0].T), cmap='plasma', aspect='auto', extent=extent)
+    ppl.imshow(np.abs(spec[...,0]), cmap='plasma', aspect='auto', extent=extent)
     ppl.plot([0, len(s1) / fs], [20, 20], 'k--')
     ppl.plot([0, len(s1) / fs], [40, 40], 'k--')
     ppl.plot([0, len(s1) / fs], [60, 60], 'k--')
@@ -105,17 +128,24 @@ def show_MorletSL(morletSL, scale):
     ts = _get_superlet_support(scale, 1/fs, cycle)
     ppl.plot(ts, MorletSL(cycle)(ts, scale))
 
+    
+def show_MorletTC(morletTC, scale):
+
+    M = 10 * scale * fs
+    # times to use, centred at zero
+    ts = np.arange((-M + 1) / 2.0, (M + 1) / 2.0) / fs
+    ppl.plot(ts, morletTC(ts, scale))
+    
 
 def do_superlet_cwt(data, wav, scales=None):
 
     if scales is None: 
-        # scales = _get_optimal_wavelet_scales(wav, len(s1), 1/fs)
-        scales = wav.scale_from_period(1 / freqs)
+        scales = get_optimal_wavelet_scales(scale_from_period, len(data[:,0]), 1/fs)
 
     res = cwtSL(data,
                 wav,
                 scales=scales,
-                dt=1/fs)              
+                dt=1 / fs)              
 
     ppl.figure()
     extent = [0, len(s1) / fs, freqs[-1], freqs[0]]
@@ -131,9 +161,9 @@ def do_superlet_cwt(data, wav, scales=None):
 def do_normal_cwt(data, wav, scales=None):
 
     if scales is None: 
-        # scales = _get_optimal_wavelet_scales(wav, len(s1), 1/fs)
-        scales = wav.scale_from_period(1 / freqs)
-    
+        scales = get_optimal_wavelet_scales(wav.scale_from_period,
+                                             len(data[:,0]),
+                                             1/fs)    
     res = wavelet(data,
                   preselect,
                   preselect,
@@ -148,6 +178,9 @@ def do_normal_cwt(data, wav, scales=None):
     ppl.figure()
     extent = [0, len(s1) / fs, freqs[-1], freqs[0]]
     ppl.imshow(res[:, 0, :, 0].T, cmap='plasma', aspect='auto', extent=extent)
+    ppl.plot([0, len(s1) / fs], [20, 20], 'k--')
+    ppl.plot([0, len(s1) / fs], [40, 40], 'k--')
+    ppl.plot([0, len(s1) / fs], [60, 60], 'k--')
 
     return res[:, 0, :, :].T
 
--- test_frontend.py
+++ test_frontend.py
@@ -6,22 +6,27 @@ from syncopy.specest import freqanalysis
 from syncopy.tests.misc import generate_artificial_data
 
 tdat = generate_artificial_data()
-trials = [t for t in tdat.trials]
 
 # test mtmfft analysis
 r_mtm = freqanalysis(tdat)
 
-
 toi_ival = np.linspace(-0.5, 1, 100)
+
 #toi_ival = [0,0.2,0.5,1]
+toi_ival = 'all'
+foi = np.logspace(-1, 2.6, 25)
 # test classical wavelet analysis
 r_wav = freqanalysis(tdat, method="wavelet",
                      toi=toi_ival,
-                     output='abs') # , foilim=[5, 500])
-res_wtrials = [t for t in r_wav.trials]
+                     output='abs',
+                     foi=None) #, foilim=[5, 500])
 
 # test superlet analysis
-foi = np.logspace(-1, 2.6, 25)
+r_sup = freqanalysis(tdat, method="superlet", toi=toi_ival,
+                     order_max=20, output='abs',
+                     order_min=1,
+                     c_1 = 5,
+                     adaptive=True)
 
 r_sup = freqanalysis(tdat, method="superlet", toi='all', order_max=30, foi=foi, output='abs',order_min=5, adaptive=True)
 #res_strials = [t for t in r_sup.trials]
