19c214eb4354e17bbdddf6c91817da0f0f03128f
tensionhead <gregor.moenke@embl.de>
2021-08-09 15:11:53 +0200

    FIX: Superlet Transform - Geometric Mean Exponents
    - for oder_min != 1 the normalisation exponents were wrong
    - now corrected to exponent = order_max + 1 - order_min
    - the SLT spectrum now recovers harmonic amplitudes
    
    Changes to be committed:
    	modified:   superlet.py

--- syncopy/specest/superlet.py
+++ syncopy/specest/superlet.py
@@ -115,16 +115,17 @@ def multiplicativeSLT(data_arr,
     # create the complete multiplicative set spanning
     # order_min - order_max
     cycles = c_1 * np.arange(order_min, order_max + 1)
+    order_num = order_max + 1 - order_min # number of different orders
     SL = [MorletSL(c) for c in cycles]
 
     # lowest order
     gmean_spec = cwtSL(data_arr, SL[0], scales, dt)
-    gmean_spec = np.power(gmean_spec, 1 / order_max)
+    gmean_spec = np.power(gmean_spec, 1 / order_num)
 
     for wavelet in SL[1:]:
         
         spec = cwtSL(data_arr, wavelet, scales, dt)
-        gmean_spec *= np.power(spec, 1 / order_max)
+        gmean_spec *= np.power(spec, 1 / order_num)
 
     return gmean_spec
 
@@ -159,7 +160,7 @@ def FASLT(data_arr,
 
     # every scale needs a different exponent
     # for the geometric mean
-    exponents = 1 / orders
+    exponents = 1 / (orders - order_min + 1)
 
     # which frequencies/scales use the same integer orders
     order_jumps = np.where(np.diff(orders_int))[0]
