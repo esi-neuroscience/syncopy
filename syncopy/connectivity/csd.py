8bab75133b865faff361e3bd047057128121625f
tensionhead <gregor.moenke@embl.de>
2021-09-01 15:22:59 +0200

    NEW: Vectorized CSDs and Coherencies
    Changes to be committed:
    	modified:   csd.py

--- syncopy/connectivity/csd.py
+++ syncopy/connectivity/csd.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# Cross Spectral Densities and Coherency
+# Cross Spectral Density and Coherency
 #
 
 # Builtin/3rd party package imports
 import numpy as np
-from scipy import signal
-import itertools
 
 # syncopy imports
 from syncopy.specest.mtmfft import mtmfft
 
 
-def csd(data_arr, samplerate, taper="hann", taperopt={}, norm=False, naive=True):
+def csd(data_arr, samplerate, taper="hann", taperopt={}, norm=False):
 
     """
     Cross spectral density (CSD) estimate between all channels
@@ -26,76 +24,47 @@ def csd(data_arr, samplerate, taper="hann", taperopt={}, norm=False, naive=True)
     If normalization is required (`norm=True`) the respective coherencies
     are returned.
 
+    Parameters
+    ----------
+    data_arr : (K,N) :class:`numpy.ndarray`
+        Uniformly sampled multi-channel time-series data
+        The 1st dimension is interpreted as the time axis,
+        columns represent individual channels.
+    norm : bool
+        Set to `True` to normalize the cross spectra 
+
+    Returns
+    -------    
+    CSD_ij : (N, N, M) :class:`numpy.ndarray`
+        Cross spectral densities or coherencies if `norm=True`. 
+        `M = K // 2 + 1` is the number of Fourier frequency bins, 
+        `N` corresponds to number of input channels.
+
     See also
     --------
     mtmfft : :func:`~syncopy.specest.mtmfft.mtmfft`
              (Multi-)tapered Fourier analysis
 
     """
-
     nSamples, nChannels = data_arr.shape
 
     # has shape (nTapers x nFreq x nChannels)
     specs, freqs = mtmfft(data_arr, samplerate, taper, taperopt)
 
-    # has shape (nChannels x nChannels x nFreq)
-    output = np.zeros((nChannels, nChannels, freqs.size))
+    # outer product along channel axes
+    # has shape (nTapers x nFreq x nChannels x nChannels)        
+    CSD_ij = specs[:, :, np.newaxis, :] * specs[:, :, :, np.newaxis].conj()
     
-    # somewhat vectorized - not really fast :/                
+    # average tapers and transpose:
+    # now has shape (nChannels x nChannels x nFreq)        
+    CSD_ij = np.real(CSD_ij.mean(axis=0).T)
+    
     if norm:
         # main diagonal: auto spectrum for each taper and averaging
-        diag = np.multiply(specs, specs.conj()).mean(axis=0)
-        # output[range(nChannels), range(nChannels), :] = np.real(diag.T)
-        diag = np.real(diag).T
-
-        for i in range(nChannels):
-            idx = slice(i, nChannels)
-            row = np.multiply(specs[..., np.tile(i, nChannels - i)],
-                              specs.conj()[..., idx])
-
-            # normalization
-            denom = np.multiply(np.tile(diag[i], ((nChannels - i), 1)), diag[i:])
-            row = row.mean(axis=0).T / np.sqrt(denom)
-            output[i, i:, ...] = np.real(row)
+        # has shape (nChannels x nFreq)        
+        diag = CSD_ij.diagonal()
+        # get the needed product pairs of the autospectra
+        Ciijj = np.sqrt(diag[:, :, None] * diag[:, None, :]).T
+        CSD_ij = CSD_ij / Ciijj
         
-    elif naive:
-        for i in range(nChannels):
-            for j in range(i, nChannels):
-                output[i, j] = np.real(specs[:, :, i] * specs[:, :, j].conj()).mean(axis=0)
-                output[j, i] = output[i, j]
-                
-    else:
-        # outer product along channel axis
-        output = specs[:, :, np.newaxis, :] * specs[:, :, :, np.newaxis].conj()
-        # average tapers
-        output = np.real(output.mean(axis=0).T)
-
-    return output
-
-
-# dummy input
-a = np.ones((10, 3)) * np.arange(1,4)
-abig = np.ones((100, 500)) * np.arange(1,501)
-# dummy mtmfft result
-# b = np.arange(1, 4) * np.ones((2,10,3)).astype('complex')
-# dummt csd matrix
-c = np.ones((5, 5, 10))
-
-# for timing
-
-def vectorized(arr):
-    nSamples, nChannels = arr.shape
-
-    return (arr[:, np.newaxis, :] * arr[:, :, np.newaxis]).T
-
-def arr_loop(arr):
-    nChannels = arr.shape[1]
-    output = np.zeros((nChannels, nChannels, arr.shape[0]))
-    
-    for i in range(nChannels):
-        for j in range(i, nChannels):
-            output[i, j] = np.real(arr[:, i] * arr[:, j])
-            output[j, i] = output[i, j]
-
-    return output
-    
+    return CSD_ij, freqs
