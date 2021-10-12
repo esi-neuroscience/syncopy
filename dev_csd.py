e3264b6d2204f3d16b74659ecd751af94b381fa1
tensionhead <tensionheadx@gmail.com>
2021-10-12 13:14:05 +0200

    WIP: Cross-Correlations

--- dev_csd.py
+++ dev_csd.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.signal import csd as sci_csd
+import scipy.signal as sci
 from syncopy.connectivity.single_trial_compRoutines import cross_spectra_cF
 import matplotlib.pyplot as ppl
 
@@ -7,6 +8,8 @@ import matplotlib.pyplot as ppl
 # white noise ensemble
 nSamples = 1000
 fs = 1000
+tvec = np.arange(nSamples) / fs
+
 nChannels = 5
 data1 = np.random.randn(nSamples, nChannels)
 
@@ -31,49 +34,70 @@ def sci_est(x, y, nper, norm=False):
     return (freqs1, np.abs(csd1)), (freqs2, np.abs(csd2))
     
 
-freqs, CS = cross_spectra_cF(data1, fs, taper='bartlett')
-freqs, CS2 = cross_spectra_cF(data1, fs, taper='dpss',
-                              taperopt={'Kmax' : 10, 'NW' : 3}, norm=True)
+def mtm_csd_harmonics():
 
-freqs, CS3 = cross_spectra_cF(data1, fs, taper='dpss',
-                              taperopt={'Kmax' : 20, 'NW' : 3}, norm=True)
+    omegas = np.array([30, 80]) * 2 * np.pi
+    phase_shifts = np.array([0, np.pi / 2, np.pi])
+
+    NN = 50
+    res = np.zeros((nSamples // 2 + 1, NN))
+    eps = 1
+    Kmax = 5
+    data = [np.sum([np.cos(om * tvec + ps) for om in omegas], axis=0) for ps in phase_shifts]
+    data = np.array(data).T
+    data = 5 * (data + np.random.randn(nSamples, 3) * eps)
+    
+    for i in range(NN):
+
+        freqs, CS = cross_spectra_cF(data, fs, taper='bartlett')
+        freqs, CS2 = cross_spectra_cF(data, fs, taper='dpss', taperopt={'Kmax' : Kmax, 'NW' : 6}, norm=True)
+
+        res[:, i] = np.abs(CS2[0, 0, 1, :])
+
+    q1 = np.percentile(res, 25, axis=1)
+    q3 = np.percentile(res, 75, axis=1)
+    med = np.percentile(res, 50, axis=1)
+
+    fig, ax = ppl.subplots(figsize=(6,4), num=None)
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_ylabel('coherence')
+    ax.set_ylim((-.02,1.05))
+    ax.set_title(f'MTM coherence, {Kmax} tapers, SNR={1/eps**2}')
+
+    c = 'cornflowerblue'
+    ax.plot(freqs, med, lw=2, alpha=0.8, c=c)
+    ax.fill_between(freqs, q1, q3, color=c, alpha=0.3)
 
 
+sig1 = np.cos(2 * np.pi * 30 * tvec)
+sig2 = np.sin(2 * np.pi * 30 * tvec)
 
-# harmonics
-tvec = np.arange(nSamples) / fs
-omegas = np.array([30, 80]) * 2 * np.pi
-phase_shifts = np.array([0, np.pi / 2, np.pi])
+mode = 'same'
+r = sci.correlate(sig1, sig2, mode=mode, method='fft')
+r2 = sci.fftconvolve(sig1, sig2[::-1], mode=mode)
+assert np.all(r == r2)
 
-NN = 50
-res = np.zeros((len(freqs), NN))
-eps = 1
-Kmax = 5
-for i in range(NN):
+lags = np.arange(-nSamples // 2, nSamples // 2)
 
-    data2 = [np.sum([np.cos(om * tvec + ps) for om in omegas], axis=0) for ps in phase_shifts]
-    data2 = np.array(data2).T
-    data2 = 5 * (data2 + np.random.randn(nSamples, 3) * eps)
+ppl.figure(1)
+ppl.xlabel('lag (s)')
+ppl.ylabel('convolution result')
 
-    x2 = data2[:, 0]
-    y2 = data2[:, 1]
+ppl.plot(lags, r2, lw = 1.5)
+#  ppl.xlim((490,600))
 
-    freqs, CS = cross_spectra_cF(data2, fs, taper='bartlett')
-    freqs, CS2 = cross_spectra_cF(data2, fs, taper='dpss', taperopt={'Kmax' : Kmax, 'NW' : 6}, norm=True)
+norm = np.arange(nSamples // 2, nSamples) / 2
+norm = np.r_[norm, norm[::-1]]
 
-    res[:, i] = np.abs(CS2[0, 0, 1, :])
+ppl.figure(2)
+ppl.xlabel('lag (s)')
+ppl.ylabel('correlation')
+ppl.plot(lags, r2 / norm, lw = 1.5)
 
-q1 = np.percentile(res, 25, axis=1)
-q3 = np.percentile(res, 75, axis=1)
-med = np.percentile(res, 50, axis=1)
 
-fig, ax = ppl.subplots(figsize=(6,4), num=None)
-ax.set_xlabel('frequency (Hz)')
-ax.set_ylabel('coherence')
-ax.set_ylim((-.02,1.05))
-ax.set_title(f'MTM coherence, {Kmax} tapers, SNR={1/eps**2}')
+sig3 = np.cos(2 * np.pi * 30 * tvec + np.pi)
+data = np.c_[sig1, sig2, sig3, sig1]
 
-c = 'cornflowerblue'
-ax.plot(freqs, med, lw=2, alpha=0.8, c=c)
-ax.fill_between(freqs, q1, q3, color=c, alpha=0.3)
+rr = sci.fftconvolve(data, data[::-1, ::-1], mode='same')
+
 
