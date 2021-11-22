626032875f1280cda551f6a8bc28835718f2a61a
tensionhead <tensionheadx@gmail.com>
2021-11-22 10:37:33 +0100

    WIP : Cleanup unneeded dev files
    Changes to be committed:
    	deleted:    dev_csd.py
    	modified:   dev_frontend.py
    	deleted:    testdev_backend.py
    	deleted:    testdev_frontend.py

--- dev_frontend.py
+++ dev_frontend.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.signal as sci
 
-from syncopy.datatype import CrossSpectralData, padding, SpectralData
+from syncopy.datatype import CrossSpectralData, padding, SpectralData, AnalogData
 from syncopy.connectivity.ST_compRoutines import cross_spectra_cF, ST_CrossSpectra
 from syncopy.connectivity.ST_compRoutines import cross_covariance_cF
 from syncopy.connectivity import connectivityanalysis
@@ -21,29 +21,31 @@ sdict1 = {"trials": [0], 'channels' : ['channel1'], 'toi': np.arange(-1, 1, 0.00
 
 # no problems here..
 coherence = connectivityanalysis(data=tdat,
-                                foilim=None,
-                                output='pow')
+                                 foilim=None,
+                                 output='pow')
+
+# D = SpectralData(dimord=['freq','test1','test2','taper'])
+# D2 = AnalogData(dimord=['freq','test1'])
 
 # a lot of problems here..
 # correlation = connectivityanalysis(data=tdat, method='corr', keeptrials=False)
 
-
 # the hard wired dimord of the cF
 dimord = ['None', 'freq', 'channel_i', 'channel_j']
 
-# res = freqanalysis(data=tdat,
-#                    method='mtmfft',
-#                    samplerate=tdat.samplerate,
-# #                   order_max=20,
-# #                   foilim=foilim,
-# #                   foi=np.arange(502),
-#                    output='pow',
-# #                   polyremoval=1,
-#                    t_ftimwin=0.5,
-#                    keeptrials=False,
-#                    taper='dpss',
-#                    nTaper = 11,
-#                    tapsmofrq=5,
-#                    keeptapers=True,
-#                    parallel=False, # try this!!!!!!
-#                    select={"trials" : [0,1]})
+res = freqanalysis(data=tdat,
+                   method='mtmfft',
+                   samplerate=tdat.samplerate,
+#                   order_max=20,
+#                   foilim=foilim,
+#                   foi=np.arange(502),
+                   output='pow',
+#                   polyremoval=1,
+                   t_ftimwin=0.5,
+                   keeptrials=True,
+                   taper='dpss',
+                   nTaper = 11,
+                   tapsmofrq=5,
+                   keeptapers=True,
+                   parallel=False, # try this!!!!!!
+                   select={"trials" : [0,1]})
