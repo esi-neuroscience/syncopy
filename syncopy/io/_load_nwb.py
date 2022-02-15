44d673adc7a12f4f0e8cdd1a48a9f44fe2dc7864
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2022-02-15 11:50:49 +0100

    CHG: Added comment
    - included comment to clarify `EventData` instantiation
    
     On branch nwb
     Changes to be committed:
    	modified:   syncopy/io/_load_nwb.py

--- syncopy/io/_load_nwb.py
+++ syncopy/io/_load_nwb.py
@@ -179,6 +179,9 @@ def load_nwb(filename, memuse=3000):
         h5evt = h5py.File(evtData.filename, mode="w")
         evtDset = h5evt.create_dataset("data", dtype=np.result_type(*ttlDtypes),
                                        shape=(ttlVals[0].data.size, 3))
+        # Column 1: sample indices
+        # Column 2: TTL pulse values
+        # Column 3: TTL channel markers
         evtDset[:, 0] = ((ttlChans[0].timestamps[()] - tStarts[0]) / ttlChans[0].timestamps__resolution).astype(np.intp)
         evtDset[:, 1] = ttlVals[0].data[()]
         evtDset[:, 2] = ttlChans[0].data[()]
