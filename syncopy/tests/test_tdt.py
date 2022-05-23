6314a72028ca4227d9e81973dd42833d782c1b1a
Gregor Moenke <tensionheadx@gmail.com>
2022-05-23 16:12:24 +0200

    NEW: Small test script to showcase/improve the tdt reader
    Changes to be committed:
    	new file:   syncopy/tests/test_tdt.py

---
+++ syncopy/tests/test_tdt.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Simple script for testing/adjusting the tdt reader 
+#
+
+# Builtin/3rd party package imports
+import numpy as np
+
+# Add SynCoPy package to Python search path
+import os
+import sys
+
+# Import package
+import syncopy as spy
+
+data_path = '/cs/scratch/syncopy/Tdt_reader'
+out_path = data_path
+TDTdata = spy.io.load_tdt.ESI_TDTdata(data_path,
+                                      out_path,
+                                      combined_data_filename='sth',
+                                      subtract_median=False)
+    
+# how to go on from here to end up with a Syncopy Data object?
+# ...
