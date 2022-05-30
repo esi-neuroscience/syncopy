f5b75954558720be14555b0495f37093964db1e0
tensionhead <tensionheadx@gmail.com>
2022-05-30 15:58:14 +0200

    NEW: Test scipy resampling with homegrown firws
    - this shows superior spectral response, yet needs a normalization
    tweak :/
    - also fixed sampling rate mixup.. (rs_fs, ds_fs)
    
    On branch resampling
    Your branch is up to date with 'origin/resampling'.
    
    Changes to be committed:
    	modified:   syncopy/tests/backend/test_resampling.py

--- syncopy/tests/backend/test_resampling.py
+++ syncopy/tests/backend/test_resampling.py
@@ -37,10 +37,9 @@ def test_resample():
 
     ds_fs = orig_fs // 2 # half the original sampling rate
     # make sure it's an integer division
-    assert orig_fs % ds_fs == 0    
+    assert orig_fs % ds_fs == 0
     ds_data = [resampling.downsample(signal, orig_fs, ds_fs) for signal in data]
     ds_power, ds_freqs = trl_av_power(ds_data, nSamples, ds_fs)
-
     # w/o low-pass filtering, the high frequencies above the
     # new Nyquist frequency wrap around and give a gain
     # directly proportional to the ratio orig_fs / ds_fs
@@ -51,11 +50,11 @@ def test_resample():
     # -- test simple downsampling with low-pass filtering --
 
     # design filter with cut off at new Nyquist
-    lpfilter = firws.design_wsinc("blackman", order=nSamples, f_c=0.5 / fs_ratio)
+    lpfilter = firws.design_wsinc("hamming", order=nSamples, f_c=0.5 / fs_ratio)
     # apply to all signals BEFORE downsampling
     lp_data = [firws.apply_fir(signal, lpfilter) for signal in data]
     ds_lp_data = [resampling.downsample(signal, orig_fs, ds_fs) for signal in lp_data]
-    ds_lp_power, ds_freqs = trl_av_power(ds_lp_data, nSamples, ds_fs)
+    ds_lp_power, ds_lp_freqs = trl_av_power(ds_lp_data, nSamples, ds_fs)
 
     # with low-pass filtering, the high frequencies above the
     # new Nyquist frequency are removed and hence there should
@@ -69,12 +68,21 @@ def test_resample():
     # make sure we have a non-integer division
     assert orig_fs % rs_fs > 1  # strictly > 0 would be enough..
 
-    rs_data = [resampling.resample(signal, orig_fs, ds_fs) for signal in data]
-    rs_power, rs_freqs = trl_av_power(rs_data, nSamples, ds_fs)
+    rs_data = [resampling.resample(signal, orig_fs, rs_fs) for signal in data]
+    rs_power, rs_freqs = trl_av_power(rs_data, nSamples, rs_fs)
 
     # here we have implicit FIR filtering built in,
     # hence there should be again no gain
     gain = rs_power.mean() / orig_power.mean()
+    assert 0.95 < gain < 1.05
+
+    # use home grown firws, needs other normalization?!
+    lpfilter2 = firws.design_wsinc("hamming", order=nSamples,
+                                   f_c=0.5 * rs_fs / orig_fs) * 1 / np.sqrt(2)
+    rs_data2 = [resampling.resample(signal, orig_fs, rs_fs, window=lpfilter2)
+                for signal in data]
+    rs_power2, rs_freqs2 = trl_av_power(rs_data2, nSamples, rs_fs)
+    gain = rs_power2.mean() / orig_power.mean()
     assert 0.95 < gain < 1.05
 
     # -- plot all the power spectra --
@@ -85,8 +93,9 @@ def test_resample():
 
     ax.plot(orig_freqs, orig_power, label='original', lw=1.5, alpha=0.5)
     ax.plot(ds_freqs, ds_power, label='downsampled')
-    ax.plot(ds_freqs, ds_lp_power, label='downsampled + FIRWS')
+    ax.plot(ds_lp_freqs, ds_lp_power, label='downsampled + FIRWS')
     ax.plot(rs_freqs, rs_power, label='resample_poly')
+    ax.plot(rs_freqs2, rs_power2, label='resample_poly + FIRWS')
     ax.set_ylim((0, ds_power.mean() * 1.2))
     ax.legend()
 
@@ -97,8 +106,8 @@ def trl_av_power(data, nSamples, fs, tapsmofrq=1):
     for signal in data:
         NW, Kmax = mtmfft._get_dpss_pars(1, nSamples, fs)
         ftr, freqs = mtmfft.mtmfft(signal, samplerate=fs,
-                                        taper='dpss',
-                                        taper_opt={'Kmax' : Kmax, 'NW' : NW})
+                                   taper='dpss',
+                                   taper_opt={'Kmax' : Kmax, 'NW' : NW})
         # no tapers so 1st axis is singleton
         power.append(np.real(ftr * ftr.conj()).mean(axis=0))
     power = np.mean(power, axis=0)
