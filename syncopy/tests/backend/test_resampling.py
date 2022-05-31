eb2c41aa70754c6f7c7f2a4a05b399e084983727
tensionhead <tensionheadx@gmail.com>
2022-05-31 09:55:47 +0200

    CHG: Tighten resample_poly + own firws test criteria
    - this strategy produces very good results, hence we can be more
    thorough with testing
    
    On branch resampling
    Your branch is up to date with 'origin/resampling'.
    
    Changes to be committed:
    	modified:   syncopy/tests/backend/test_resampling.py

--- syncopy/tests/backend/test_resampling.py
+++ syncopy/tests/backend/test_resampling.py
@@ -35,7 +35,7 @@ def test_resample():
 
     # -- test simple downsampling w/o low-pass filtering --
 
-    ds_fs = orig_fs // 2 # half the original sampling rate
+    ds_fs = orig_fs // 2  # half the original sampling rate
     # make sure it's an integer division
     assert orig_fs % ds_fs == 0
     ds_data = [resampling.downsample(signal, orig_fs, ds_fs) for signal in data]
@@ -51,6 +51,7 @@ def test_resample():
 
     # design filter with cut off at new Nyquist
     lpfilter = firws.design_wsinc("hamming", order=nSamples, f_c=0.5 / fs_ratio)
+
     # apply to all signals BEFORE downsampling
     lp_data = [firws.apply_fir(signal, lpfilter) for signal in data]
     ds_lp_data = [resampling.downsample(signal, orig_fs, ds_fs) for signal in lp_data]
@@ -60,7 +61,7 @@ def test_resample():
     # new Nyquist frequency are removed and hence there should
     # be no gain
     gain = ds_lp_power.mean() / orig_power.mean()
-    assert 0.95 < gain < 1.05
+    assert 0.98 < gain < 1.02
 
     # -- test non-trivial resampling --
 
@@ -73,31 +74,42 @@ def test_resample():
 
     # here we have implicit FIR filtering built in,
     # hence there should be again no gain
+    # NOTE: There is however an overshoot and ringing
+    # with the default (implicit) FIR filter
+    # Hence we need more relaxed gain checking here (5%)
     gain = rs_power.mean() / orig_power.mean()
     assert 0.95 < gain < 1.05
 
     # use home grown firws, needs other normalization?!
-    lpfilter2 = firws.design_wsinc("hamming", order=nSamples,
-                                   f_c=0.5 * rs_fs / orig_fs) * 1 / np.sqrt(2)
-    rs_data2 = [resampling.resample(signal, orig_fs, rs_fs, window=lpfilter2)
-                for signal in data]
+    lpfilter2 = (
+        firws.design_wsinc("hamming",
+                           order=nSamples,
+                           f_c=0.5 * rs_fs / orig_fs)
+        * 1 / np.sqrt(2)
+    )
+    rs_data2 = [
+        resampling.resample(signal, orig_fs, rs_fs, window=lpfilter2) for signal in data
+    ]
     rs_power2, rs_freqs2 = trl_av_power(rs_data2, nSamples, rs_fs)
     gain = rs_power2.mean() / orig_power.mean()
-    assert 0.95 < gain < 1.05
+    # NOTE: this works very well and we can
+    # give again harder constraints on the gain (2%)
+    assert 0.98 < gain < 1.02
 
     # -- plot all the power spectra --
 
     fig, ax = ppl.subplots()
-    ax.set_xlabel('frequency (Hz)')
-    ax.set_ylabel('power (a.u.)')
+    ax.set_xlabel("frequency (Hz)")
+    ax.set_ylabel("power (a.u.)")
 
-    ax.plot(orig_freqs, orig_power, label='original', lw=1.5, alpha=0.5)
-    ax.plot(ds_freqs, ds_power, label='downsampled')
-    ax.plot(ds_lp_freqs, ds_lp_power, label='downsampled + FIRWS')
-    ax.plot(rs_freqs, rs_power, label='resample_poly')
-    ax.plot(rs_freqs2, rs_power2, label='resample_poly + FIRWS')
+    ax.plot(orig_freqs, orig_power, label="original", lw=1.5, alpha=0.5)
+    ax.plot(ds_freqs, ds_power, label="downsampled")
+    ax.plot(ds_lp_freqs, ds_lp_power, label="downsampled + FIRWS")
+    ax.plot(rs_freqs, rs_power, label="resample_poly")
+    ax.plot(rs_freqs2, rs_power2, label="resample_poly + FIRWS")
     ax.set_ylim((0, ds_power.mean() * 1.2))
     ax.legend()
+    fig.tight_layout()
 
 
 def trl_av_power(data, nSamples, fs, tapsmofrq=1):
@@ -105,10 +117,10 @@ def trl_av_power(data, nSamples, fs, tapsmofrq=1):
     power = []
     for signal in data:
         NW, Kmax = mtmfft._get_dpss_pars(1, nSamples, fs)
-        ftr, freqs = mtmfft.mtmfft(signal, samplerate=fs,
-                                   taper='dpss',
-                                   taper_opt={'Kmax' : Kmax, 'NW' : NW})
-        # no tapers so 1st axis is singleton
+        ftr, freqs = mtmfft.mtmfft(
+            signal, samplerate=fs, taper="dpss", taper_opt={"Kmax": Kmax, "NW": NW}
+        )
         power.append(np.real(ftr * ftr.conj()).mean(axis=0))
+    # trial averaging
     power = np.mean(power, axis=0)
     return power, freqs
