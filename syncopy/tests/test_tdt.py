282fbf8e08c7230c737f220463d809c803ce22b5
kajal5888 <kajal5888@yahoo.co.in>
2022-06-22 15:50:40 +0200

    Finished test script for testing

--- syncopy/tests/test_tdt.py
+++ syncopy/tests/test_tdt.py
@@ -13,12 +13,13 @@ import sys
 # Import package
 import syncopy as spy
 
-data_path = '/cs/slurm/syncopy/Tdt_reader'
+data_path = '/mnt/hpc/slurm/syncopy/Tdt_reader/'
 out_path = data_path
-TDTdata = spy.io.load_tdt.ESI_TDTdata(data_path,
-                                      out_path,
-                                      combined_data_filename='sth',
-                                      subtract_median=False)
-    
 # how to go on from here to end up with a Syncopy Data (spy.AnalogData) object?
 # ...
+
+TDT_Load_Info = spy.io.load_tdt.ESI_TDTinfo(data_path)
+DataInfo_loaded = TDT_Load_Info.load_tdt_info()
+Files = TDT_Load_Info.get_files('.sev', 'DivAtt_session-25_LFPs')
+TDT_Data = spy.io.load_tdt.ESI_TDTdata(data_path, out_path, 'sth', subtract_median=False, channels=None, export=True)
+Data_Syncopy = TDT_Data.data_aranging(Files, DataInfo_loaded)
