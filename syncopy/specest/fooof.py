a2fb72ca0893164f235cfeaeb92be7d0aad67898
Tim Schäfer <ts+code@rcmd.org>
2022-06-24 11:35:55 +0200

    WIP: add FOOOF file with started docs

---
+++ syncopy/specest/fooof.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# Parameterization of neural power spectra with FOOOF - fitting oscillations & one over f.
+#
+#
+#
+
+# Builtin/3rd party package imports
+import numpy as np
+from scipy import signal
+
+# Syncopy imports
+from syncopy.shared.errors import SPYValueError
+
+# Constants
+available_fooof_out_types = ['spec_periodic', 'fit_gaussians', 'fit_aperiodic']
+
+def fooof(data_arr,
+           freqs,
+           fooof_opt= None,
+           out_type='spec_periodic'):
+    """
+    Parameterization of neural power spectra using 
+    the FOOOF mothod by Donoghue et al: fitting oscillations & one over f.
+
+    Parameters
+    ----------
+    data_arr : 3D :class:`numpy.ndarray`
+         Complex has shape ``(nTapers x nFreq x nChannels)``, obtained from :func:`syncopy.specest.mtmfft` output.
+    freqs : 1D :class:`numpy.ndarray`
+         Array of Fourier frequencies, obtained from mtmfft output.
+    foof_opt : dict or None
+        Additional keyword arguments passed to the `FOOOF` constructor.
+        For multi-tapering with ``taper='dpss'`` set the keys
+        `'Kmax'` and `'NW'`.
+        For further details, please refer to the
+        `FOOOF docs <https://fooof-tools.github.io/fooof/generated/fooof.FOOOF.html#fooof.FOOOF>`_
+    out_type : string
+        The requested output type, one of ``'spec_periodic'`` for the original spectrum minus the aperiodic
+        parts, ``'fit_gaussians'`` for the Gaussians fit to the original spectrum minus the aperiodic parts, or
+        ``'fit_aperiodic'``.
+
+    Returns
+    -------
+    Depends on the value of parameter ``'out_type'``.
+    TODO: describe here.
+
+    References
+    -----
+    Donoghue T, Haller M, Peterson EJ, Varma P, Sebastian P, Gao R, Noto T, Lara AH, Wallis JD,
+    Knight RT, Shestyuk A, & Voytek B (2020). Parameterizing neural power spectra into periodic
+    and aperiodic components. Nature Neuroscience, 23, 1655-1665.
+    DOI: 10.1038/s41593-020-00744-x
+    """
+
+    # attach dummy channel axis in case only a
+    # single signal/channel is the input
+    if data_arr.ndim < 2:
+        data_arr = data_arr[:, np.newaxis]
+
+    if fooof_opt is None:
+        fooof_opt = {}
+
+    if out_type not in available_fooof_out_types:
+        lgl = "'" + "or '".join(opt + "' " for opt in available_fooof_out_types)
+        raise SPYValueError(legal=lgl, varname="out_type", actual=out_type)
+    
+    return data_arr
+
