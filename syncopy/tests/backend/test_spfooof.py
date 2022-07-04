615156a80d1db8a171fb4a580bef13dcb3bfe0c0
Tim Schäfer <ts+code@rcmd.org>
2022-07-04 12:01:12 +0200

    WIP: refactor tests to class, test exceptions

--- syncopy/tests/backend/test_spfooof.py
+++ syncopy/tests/backend/test_spfooof.py
@@ -11,6 +11,8 @@ from syncopy.specest.spfooof import spfooof
 from fooof.sim.gen import gen_power_spectrum
 from fooof.sim.utils import set_random_seed
 
+from syncopy.shared.errors import SPYValueError
+
 import matplotlib.pyplot as plt
 
 
@@ -18,23 +20,36 @@ def _plotspec(f, p):
     plt.plot(f, p)
     plt.show()
 
-@pytest.fixture
-def spectrum():
+
+def _power_spectrum():
     set_random_seed(21)    
     freqs, powers = gen_power_spectrum([3, 40], [1, 1],
                                        [[10, 0.2, 1.25], [30, 0.15, 2]])
     return (freqs, powers)
 
 
-def test_fooof_ouput_fooof(spectrum):
-    """
-    Tests fooof with output 'fooof'. This will return the full, foofed spectrum.
-    """        
-    freqs, powers = spectrum    
+class TestSpfooof():
 
-    # _plotspec(freqs1, powers)
-    spectra, details = spfooof(powers, fooof_settings={'in_freqs': freqs, 'freq_range': None}, out_type = 'fooof')
+    freqs, powers = _power_spectrum()
 
-    assert spectra.shape == (freqs.size, )
-    assert all (key in details for key in ("aperiodic_params", "n_peaks", "r_squared", "error", "settings_used"))
+    def test_spfooof_ouput_fooof(self, freqs=freqs, powers=powers):
+        """
+        Tests spfooof with output 'fooof'. This will return the full, foofed spectrum.
+        """                
+
+        # _plotspec(freqs1, powers)
+        spectra, details = spfooof(powers, fooof_settings={'in_freqs': freqs, 'freq_range': None}, out_type = 'fooof')
+
+        assert spectra.shape == (freqs.size, 1)
+        assert all (key in details for key in ("aperiodic_params", "n_peaks", "r_squared", "error", "settings_used"))
+
+
+    def test_spfooof_exceptions(self):
+
+        # The input frequencies must not be None.
+        with pytest.raises(SPYValueError) as err:
+            self.test_spfooof_ouput_fooof(freqs=None, powers=self.powers)
+            assert "input frequencies are required and must not be None" in str(err)
+
+
 
