70f185a603f17579f11c071bf8eb56db16356f2a
tensionhead <tensionheadx@gmail.com>
2022-07-21 11:53:11 +0200

    WIP: Index gymnastics for channel-unit combos
    - finally reduce came to the rescue
    - single trial psth writes into global output shaped array
    
    Changes to be committed:
    	modified:   syncopy/spikes/psth.py

--- syncopy/spikes/psth.py
+++ syncopy/spikes/psth.py
@@ -3,7 +3,7 @@ from scipy.stats import iqr
 
 
 def psth(trl_dat,
-         cu_combs,
+         cu_combos,
          trl_start=0,
          onset=0,
          bins=None,
@@ -21,7 +21,7 @@ def psth(trl_dat,
     ----------
     trl_dat : :class:`~np.ndarray`
         Single trial spike data with shape (nEvents x 3)
-    cu_combs : :class:`~np.ndarray`
+    cu_combos : :class:`~np.ndarray`
         All (sorted) numeric channel-unit combinations to bin for
         arangend in a (N, 2) shaped array, where each row is
         one unique combination (say [4, 1] for channel4 - unit1)
@@ -45,15 +45,11 @@ def psth(trl_dat,
     `Rice Rule <https://en.wikipedia.org/wiki/Histogram#Rice_Rule>`_ on Wikipedia
     """
 
-    # sort according to channels
-    # to sort into correct bins
-    idx = np.argsort(trl_dat[:, 1])
 
     # for readability
-    samples = trl_dat[idx, 0]
-    channels = trl_dat[idx, 1]
-    units = trl_dat[idx, 2]
-    nChannels = np.unique(channels).size
+    samples = trl_dat[:, 0]
+    channels = trl_dat[:, 1]
+    units = trl_dat[:, 2]
 
     # get relative spike times, no time-sorting needed for histograms!
     times = _calc_time(samples, trl_start, onset, samplerate)
@@ -65,31 +61,37 @@ def psth(trl_dat,
     else:
         nBins = len(bins) - 1
 
-    # globally available units
-    unique_units = np.unique(cu_combs[:, 1])
-    # globally available channels
-    unique_chans = np.unique(cu_combs[:, 0])
-    nChannels = len(unique_chans)
+    # this could mean here [chan1, chan5, chan10]!
+    chan_vec = np.unique(channels)
+    # now the ith channel bin maps to the ith available channel
+    # (0, 1, 2) -> (1, 5, 10)
+    bins = [bins, np.arange(chan_vec.size + 1)]
 
-    # attach 2nd bin dimension along channels
-    bins = [bins, np.arange(nChannels + 1)]
+    # this is the global(!) output shape - some columns may be filled with 0s
+    # -> no firing for that chan-unit combo in this specific trial
+    counts = np.zeros((nBins, len(cu_combos)))
 
-    # counts = np.zeros((nBins, len(cu_combs) + 1))
-    counts = np.zeros((nBins, nChannels, len(unique_units)))
+    # available units in this trial
+    unique_units = np.unique(units)
 
-    # for i, comb in enumerate(cu_combs):
+    # create boolean mapping of all trial specific combinations
+    # into global output shape
+    map_cu = lambda c, u: np.all(cu_combos == [c, u], axis=1)
+    # now map with respect to unit for all single trial channels (-bins)
+    map_unit = {u: np.logical_or.reduce([map_cu(c, u) for c in chan_vec]) for u in unique_units}
+
     for i, iunit in enumerate(unique_units):
         unit_idx = (units == iunit)
-        # boolean index for specific chan-unit combination
-        # bidx = np.all(trl_dat[:, 1:] == comb, axis=1)
 
         if np.sum(unit_idx):
-            counts[..., i] = np.histogram2d(times[unit_idx],
-                                            channels[unit_idx],
-                                            bins=bins)[0]
-        # no spikes in this unit
-        else:
-            counts[..., i] = np.zeros(nBins, nChannels)
+            # over all channels, so this counts different units actually
+            unit_counts = np.histogram2d(times[unit_idx],
+                                         channels[unit_idx],
+                                         bins=bins)[0]
+            # get indices to inject the results
+            # at the right position
+            cu_idx = map_unit[iunit]
+            counts[:, cu_idx] = unit_counts
 
     return counts, bins
 
