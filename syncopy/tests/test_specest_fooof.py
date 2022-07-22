FIX: fix computation of fooof_peaks output

syncopy/tests/test_specest_fooof.py
@@ -72,6 +72,14 @@ def _show_spec_log(analog_data, save="test.png"):
         print("Saving power spectrum figure for AnalogData to '{save}'. Working directory is '{wd}'.".format(save=save, wd=os.getcwd()))
         plt.savefig(save)
 
+def spp(dt, title=None):
+    """Single panet plot with a title."""
+    if not isinstance(dt, spy.datatype.base_data.BaseData):
+        raise ValueError("Parameter 'dt' must be a syncopy.datatype instance.")
+    fig, ax = dt.singlepanelplot()
+    if title is not None:
+        ax.set_title(title)
+    return fig, ax
 
 def _get_fooof_signal(nTrials = 100):
     """
@@ -113,7 +121,6 @@ class TestFooofSpy():
         cfg.output = "fooof"
         cfg.foilim = [1., 100.]
         return cfg
-
 
     def test_output_fooof_fails_with_freq_zero(self):
         """ The fooof package ignores input values of zero frequency, and shortens the output array
@@ -161,8 +168,8 @@ class TestFooofSpy():
         assert spec_dt.cfg['freqanalysis']['output'] == 'fooof'
 
         # Plot it.
-        _plot_powerspec_linear(freqs=spec_dt.freq, powers=spec_dt.data[0, 0, :, 0], title="fooof full model, for ar1 data (linear scale)")
-        spec_dt.singlepanelplot()
+        #_plot_powerspec_linear(freqs=spec_dt.freq, powers=spec_dt.data[0, 0, :, 0], title="fooof full model, for ar1 data (linear scale)")
+        #spp(spec_dt, "FOOOF full model")
         #plt.savefig("spp.png")
 
     def test_output_fooof_aperiodic(self):
@@ -182,8 +189,8 @@ class TestFooofSpy():
         assert spec_dt.data.ndim == 4
         assert spec_dt.data.shape == (1, 1, 100, 1)
         assert not np.isnan(spec_dt.data).any()
-        _plot_powerspec_linear(freqs=spec_dt.freq, powers=np.ravel(spec_dt.data), title="fooof aperiodic, for ar1 data (linear scale)")
-        spec_dt.singlepanelplot()
+        #_plot_powerspec_linear(freqs=spec_dt.freq, powers=np.ravel(spec_dt.data), title="fooof aperiodic, for ar1 data (linear scale)")
+        #spp(spec_dt, "FOOOF aperiodic")
 
     def test_output_fooof_peaks(self):
         """Test fooof with output type 'fooof_peaks'. A spectrum containing only the peaks (actually, the Gaussians fit to the peaks) is returned."""
@@ -196,10 +203,10 @@ class TestFooofSpy():
         assert "fooof" in spec_dt._log
         assert "fooof_method = fooof_peaks" in spec_dt._log
         assert "fooof_aperiodic" not in spec_dt._log
-        _plot_powerspec_linear(freqs=spec_dt.freq, powers=np.ravel(spec_dt.data), title="fooof peaks, for ar1 data (linear scale)")
-        spec_dt.singlepanelplot()
+        #_plot_powerspec_linear(freqs=spec_dt.freq, powers=np.ravel(spec_dt.data), title="fooof peaks, for ar1 data (linear scale)")
+        #spp(spec_dt, "FOOOF peaks")
 
-    def test_outputs_from_different_fooof_methods_are_consistent(self):
+    def test_different_fooof_methods_are_consistent(self):
         """Test fooof with all output types plotted into a single plot and ensure consistent output."""
         cfg = TestFooofSpy.get_fooof_cfg()
         cfg['output'] = "pow"