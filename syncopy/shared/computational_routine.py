2c03f3060e591fa7e6c2ad7b748f187cd1008667
Tim Schaefer <ts+code@rcmd.org>
2022-08-12 14:53:43 +0200

    FIX: better warning message

--- syncopy/shared/computational_routine.py
+++ syncopy/shared/computational_routine.py
@@ -597,7 +597,7 @@ class ComputationalRoutine(ABC):
 
         # Do not spill trials on disk if they're supposed to be removed anyway
         if parallel_store and not self.keeptrials:
-            msg = "trial-averaging only supports sequential writing, disabling parallel mode!"
+            msg = "trial-averaging only supports sequential storage, disabling parallel storage mode!"
             SPYWarning(msg)
             parallel_store = False
 
