ce5f8a0efb8d894df0a3ffdd57265cb2ff74cf65
tensionhead <tensionheadx@gmail.com>
2022-08-12 18:10:31 +0200

    FIX: Construct trialdefinition of resampled data
    - there might still be some float precision edge cases lurking somewhere..
    
    Changes to be committed:
    	modified:   syncopy/preproc/compRoutines.py
    	modified:   syncopy/tests/test_resampledata.py

--- syncopy/preproc/compRoutines.py
+++ syncopy/preproc/compRoutines.py
@@ -501,14 +501,15 @@ class Downsample(ComputationalRoutine):
     def process_metadata(self, data, out):
 
         # we need to re-calculate the downsampling factor
-        factor = int(data.samplerate // self.cfg['new_samplerate'])
+        # that it actually is an 1 / integer gets checked in the frontend
+        factor = self.cfg['new_samplerate'] / data.samplerate
 
         if data.selection is not None:
             chanSec = data.selection.channel
-            trl = data.selection.trialdefinition // factor
+            trl = _resampling_trl_definition(data.selection.trialdefinition, factor)
         else:
             chanSec = slice(None)
-            trl = data.trialdefinition // factor
+            trl = _resampling_trl_definition(data.trialdefinition, factor)
 
         out.trialdefinition = trl
         # now set new samplerate
@@ -615,14 +616,14 @@ class Resample(ComputationalRoutine):
 
         # we need to re-calculate the resampling factor
         factor = self.cfg['new_samplerate'] / data.samplerate
-        trafo_trl = lambda trldef: np.ceil(trldef * factor)
+        trafo_trl = _resampling_trl_definition
 
         if data.selection is not None:
             chanSec = data.selection.channel
-            trl = trafo_trl(data.selection.trialdefinition)
+            trl = trafo_trl(data.selection.trialdefinition, factor)
         else:
             chanSec = slice(None)
-            trl = trafo_trl(data.trialdefinition)
+            trl = trafo_trl(data.trialdefinition, factor)
 
         out.trialdefinition = trl
 
@@ -812,3 +813,31 @@ class Standardize(ComputationalRoutine):
     def process_metadata(self, data, out):
 
         propagate_metadata(data, out)
+
+
+def _resampling_trl_definition(orig_trl, factor):
+
+    """
+    Construct new trialdefinition from original
+    trialdefinition and the resampling factor
+    """
+
+    # start from input trial lengths and scale
+    # and ceil them to arrive at new trial lengths
+    # important is 1st diff then ceil..
+    sinfo = orig_trl[:, :2]
+    trl_len = np.ceil(np.diff(sinfo * factor, axis=1)).squeeze()
+
+    # use ceil again to define new trial start
+    # and offset samples
+    trl_scaled = np.ceil(orig_trl * factor)
+    trl_starts = trl_scaled[:, 0]
+    offsets = trl_scaled[:, 2]
+
+    # now add new trl_len to get new trial ends
+    trl_ends = trl_starts + trl_len
+
+    # finally stack everything back together
+    trldef = np.column_stack([trl_starts, trl_ends, offsets])
+
+    return trldef
