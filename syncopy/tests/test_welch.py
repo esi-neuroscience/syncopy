92cf6a4e89c9eaa06298f6e2bf216e77580857aa
Tim Schäfer <ts+code@rcmd.org>
2022-11-28 12:28:55 +0100

    NEW: add test for error on tapsmofrq with welch

--- syncopy/tests/test_welch.py
+++ syncopy/tests/test_welch.py
@@ -7,6 +7,7 @@ import pytest
 import syncopy as spy
 import numpy as np
 from syncopy.tests.test_specest import TestMTMConvol
+from syncopy.shared.errors import SPYValueError
 
 
 class TestWelch():
@@ -16,12 +17,20 @@ class TestWelch():
 
     adata = TestMTMConvol.get_tfdata_mtmconvol()
 
-    def test_welch(self):
+    def test_welch_simple(self):
         cfg = spy.get_defaults(spy.freqanalysis)
         cfg.method = "welch"
         cfg.t_ftimwin = 0.5
         spec_dt = spy.freqanalysis(cfg, self.adata)
         assert spec_dt.data.ndim == 4
+
+    def test_welch_rejects_multitaper(self):
+        cfg = spy.get_defaults(spy.freqanalysis)
+        cfg.method = "welch"
+        cfg.t_ftimwin = 0.5
+        cfg.tapsmofrq = 1  # Activate multi-tapering, which is not allowed.
+        with pytest.raises(SPYValueError, match="tapsmofrq"):
+            spec_dt = spy.freqanalysis(cfg, self.adata)
 
 
 if __name__ == '__main__':
