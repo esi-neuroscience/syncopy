78aea15d710b6c6d85927db2e446ca2c4791148a
Tim Schaefer <ts+code@rcmd.org>
2022-12-20 13:44:04 +0100

    CHG: remove pointless test

--- syncopy/tests/test_tools.py
+++ syncopy/tests/test_tools.py
@@ -40,18 +40,6 @@ class TestTools:
         assert cfg.b == "test"
         assert cfg.c == [1, 2, 3]
 
-    def test_structdict_from_dict_copy(self):
-        my_dict = {'a': 0.5, 'b': 'test', 'c' : [1, 2, 3]}
-        cfg = spy.StructDict(my_dict)
-        assert type(cfg) == spy.shared.tools.StructDict
-        assert cfg.a == 0.5
-        assert cfg.b == "test"
-        assert cfg.c == [1, 2, 3]
-
-        cfg2 = cfg.copy()
-        assert type(cfg2) == spy.shared.tools.StructDict
-        assert cfg2.b == "test"
-
     def test_copy_Welch_cfg(self):
         from syncopy.tests.test_welch import TestWelch
         cfg = TestWelch.get_welch_cfg()
