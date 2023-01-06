0b7fb894ba262343441e7a60e191657c7e635277
Tim Schaefer <ts+code@rcmd.org>
2023-01-06 12:35:41 +0100

    NEW: add function to delete all syncopy log files in log dir

--- syncopy/shared/log.py
+++ syncopy/shared/log.py
@@ -4,8 +4,11 @@
 #
 # Note: The logging setup is done in the top-level `__init.py__` file.
 
+import os
 import logging
 import socket
+import syncopy
+import warnings
 
 
 loggername = "syncopy"  # Since this is a library, we should not use the root logger (see Python logging docs).
@@ -24,4 +27,25 @@ def get_parallel_logger():
     """
     host = socket.gethostname()
     return logging.getLogger(loggername + "_" + host)
+
+
+def delete_all_logfiles(silent=True):
+    """Delete all '.log' files in the Syncopy logging directory.
+
+    The log directory that will be emptied is `syncopy.__logdir__`.
+    """
+    logdir = syncopy.__logdir__
+    num_deleted = 0
+    if os.path.isdir(logdir):
+        filelist = [ f for f in os.listdir(logdir) if f.endswith(".log") ]
+        for f in filelist:
+            logfile = os.path.join(logdir, f)
+            try:
+                os.remove(logfile)
+                num_deleted += 1
+            except Exception as ex:
+                warnings.warn(f"Could not delete log file '{logfile}': {str(ex)}")
+    if not silent:
+        print(f"Deleted {num_deleted} log files from directory '{logdir}'.")
+
 
