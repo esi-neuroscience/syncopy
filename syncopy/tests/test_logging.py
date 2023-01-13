8b1ad4e1d8323a8da511b0b394f87caf52a2ccff
Tim Schaefer <ts+code@rcmd.org>
2023-01-13 12:19:38 +0100

    CHG: adapt logging test

--- syncopy/tests/test_logging.py
+++ syncopy/tests/test_logging.py
@@ -18,9 +18,13 @@ class TestLogging:
         assert os.path.isfile(logfile)
 
     def test_default_log_level_is_warning(self):
+
+        # Ensure the log level is at default (that user did not change SPYLOGLEVEL on test system)
+        assert os.getenv("SPYLOGLEVEL", "WARNING") == "WARNING"
+
         logfile = os.path.join(spy.__logdir__, "syncopy.log")
         assert os.path.isfile(logfile)
-        num_lines_bofore = sum(1 for line in open(logfile))
+        num_lines_initial = sum(1 for line in open(logfile)) # The log file gets appended, so it will most likely *not* be empty.
 
         # Log something with log level info and DEBUG, which should not affect the logfile.
         logger = get_logger()
@@ -29,13 +33,13 @@ class TestLogging:
 
         num_lines_after_info_debug = sum(1 for line in open(logfile))
 
-        assert num_lines_bofore == num_lines_after_info_debug
+        assert num_lines_initial == num_lines_after_info_debug
 
         # Now log something with log level WARNING
         SPYLog("I am adding a WARNING level log entry.", loglevel="WARNING")
 
         num_lines_after_warning = sum(1 for line in open(logfile))
-        assert num_lines_after_info_debug + 1 == num_lines_after_warning
+        assert num_lines_after_warning > num_lines_after_info_debug
 
 
 
