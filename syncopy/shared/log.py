ad65b9ad4c42ef83669891bfbdde5e4a2f4aef29
Tim Schaefer <ts+code@rcmd.org>
2023-01-20 16:47:26 +0100

    CHG: more logging in parallel mode, separate format

--- syncopy/shared/log.py
+++ syncopy/shared/log.py
@@ -20,6 +20,9 @@ loglevels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 
 
 def setup_logging(spydir=None):
+    """Setup logging on module initialization (in the module root level '__init__.py' file). Should not be called elsewhere."""
+
+    _addLoggingLevel('IMPORTANT', logging.INFO - 5)  # Add a new custom log level named 'IMPORTANT' between DEBUG and INFO.
 
     if os.environ.get("SPYLOGDIR"):
         syncopy.__logdir__ = os.path.abspath(os.path.expanduser(os.environ["SPYLOGDIR"]))
@@ -32,38 +35,11 @@ def setup_logging(spydir=None):
     if not os.path.exists(syncopy.__logdir__):
         os.makedirs(syncopy.__logdir__, exist_ok=True)
 
-    loglevel = os.getenv("SPYLOGLEVEL", "WARNING")
+    loglevel = os.getenv("SPYLOGLEVEL", "IMPORTANT")
     numeric_level = getattr(logging, loglevel.upper(), None)
-    if not isinstance(numeric_level, int):  # An invalid string was set as the env variable, default to WARNING.
-        warnings.warn("Invalid log level set in environment variable 'SPYLOGLEVEL', ignoring and using WARNING instead. Hint: Set one of 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'.")
-        loglevel = "WARNING"
-
-    # The logger for local/sequential stuff -- goes to terminal and to a file.
-    spy_logger = logging.getLogger('syncopy')
-    fmt = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-    sh = logging.StreamHandler(sys.stdout)
-    sh.setFormatter(fmt)
-    spy_logger.addHandler(sh)
-
-    logfile = os.path.join(syncopy.__logdir__, f'syncopy.log')
-    fh = logging.FileHandler(logfile)  # The default mode is 'append'.
-    fh.setFormatter(fmt)
-    spy_logger.addHandler(fh)
-
-    spy_logger.setLevel(loglevel)
-    spy_logger.debug(f"Starting Syncopy session at {datetime.datetime.now().astimezone().isoformat()}.")
-    spy_logger.info(f"Syncopy log level set to: {loglevel}.")
-
-    # Log to per-host files in parallel code by default.
-    # Note that this setup handles only the logger of the current host.
-    parloglevel = os.getenv("SPYPARLOGLEVEL", loglevel)
-    numeric_level = getattr(logging, parloglevel.upper(), None)
-    if not isinstance(numeric_level, int):  # An invalid string was set as the env variable, use default.
-        warnings.warn("Invalid log level set in environment variable 'SPYPARLOGLEVEL', ignoring and using WARNING instead. Hint: Set one of 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'.")
-        parloglevel = "WARNING"
-    host = platform.node()
-    parallel_logger_name = "syncopy_" + host
-    spy_parallel_logger = logging.getLogger(parallel_logger_name)
+    if not isinstance(numeric_level, int):  # An invalid string was set as the env variable, default to IMPORTANT.
+        warnings.warn("Invalid log level set in environment variable 'SPYLOGLEVEL', ignoring and using IMPORTANT instead. Hint: Set one of 'DEBUG', 'IMPORTANT', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'.")
+        loglevel = "IMPORTANT"
 
     class HostnameFilter(logging.Filter):
         hostname = platform.node()
@@ -72,16 +48,108 @@ def setup_logging(spydir=None):
             record.hostname = HostnameFilter.hostname
             return True
 
+    class SessionFilter(logging.Filter):
+        session = syncopy.__session__
+
+        def filter(self, record):
+            record.session = self.session
+            return True
+
+    # The logger for local/sequential stuff -- goes to terminal and to a file.
+    spy_logger = logging.getLogger('syncopy')
+    fmt_interactive = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')  # Interactive formatter: no hostname and session info (less clutter on terminal).
+    fmt_with_hostname = logging.Formatter('%(asctime)s - %(levelname)s - %(hostname)s - %(session)s: %(message)s')  # Log file formatter: with hostname and session info.
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(fmt_interactive)
+    spy_logger.addHandler(sh)
+
+    logfile = os.path.join(syncopy.__logdir__, f'syncopy.log')
+    fh = logging.FileHandler(logfile)  # The default mode is 'append'.
+    fh.addFilter(HostnameFilter())
+    fh.addFilter(SessionFilter())
+    fh.setFormatter(fmt_with_hostname)
+    spy_logger.addHandler(fh)
+
+    spy_logger.setLevel(loglevel)
+    spy_logger.debug(f"Starting Syncopy session at {datetime.datetime.now().astimezone().isoformat()}.")
+    spy_logger.info(f"Syncopy log level set to: {loglevel}.")
+
+    # Log to per-host files in parallel code by default.
+    # Note that this setup handles only the logger of the current host.
+    parloglevel = os.getenv("SPYPARLOGLEVEL", "INFO")
+    numeric_level = getattr(logging, parloglevel.upper(), None)
+    if not isinstance(numeric_level, int):  # An invalid string was set as the env variable, use default.
+        warnings.warn("Invalid log level set in environment variable 'SPYPARLOGLEVEL', ignoring and using IMPORTANT instead. Hint: Set one of 'DEBUG', 'IMPORTANT', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'.")
+        parloglevel = "IMPORTANT"
+    host = platform.node()
+    parallel_logger_name = "syncopy_" + host
+    spy_parallel_logger = logging.getLogger(parallel_logger_name)
+
     logfile_par = os.path.join(syncopy.__logdir__, f'syncopy_{host}.log')
     fhp = logging.FileHandler(logfile_par)  # The default mode is 'append'.
     fhp.addFilter(HostnameFilter())
+    fhp.addFilter(SessionFilter())
     spy_parallel_logger.setLevel(parloglevel)
-    fmt_with_hostname = logging.Formatter('%(asctime)s - %(levelname)s - %(hostname)s: %(message)s')
+
     fhp.setFormatter(fmt_with_hostname)
     spy_parallel_logger.addHandler(fhp)
     sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(fmt_interactive)
+
     spy_parallel_logger.addHandler(sh)
     spy_parallel_logger.info(f"Syncopy parallel logger '{parallel_logger_name}' setup to log to file '{logfile_par}' at level {loglevel}.")
+
+
+# See https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
+def _addLoggingLevel(levelName, levelNum, methodName=None):
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    """
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+       raise AttributeError('{} already defined in logging module'.format(levelName))
+    if hasattr(logging, methodName):
+       raise AttributeError('{} already defined in logging module'.format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+       raise AttributeError('{} already defined in logger class'.format(methodName))
+
+    # This method was inspired by the answers to Stack Overflow post
+    # http://stackoverflow.com/q/2183233/2988730, especially
+    # http://stackoverflow.com/a/13638084/2988730
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
 
 
 def get_logger():
