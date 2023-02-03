c7b1c452f5a15c6d893f95f427b3daef80d3f87d
Tim Schaefer <ts+code@rcmd.org>
2023-02-03 16:32:15 +0100

    NEW: add test for input

--- syncopy/tests/test_ica.py
+++ syncopy/tests/test_ica.py
@@ -6,13 +6,12 @@
 # 3rd party imports
 import pytest
 import numpy as np
-import matplotlib.pyplot as ppl
 
 # Local imports
 import dask.distributed as dd
 from syncopy.tests import synth_data as sd
 
-from syncopy.shared.errors import SPYValueError
+from syncopy.shared.errors import SPYValueError, SPYTypeError
 from syncopy.shared.tools import get_defaults, best_match
 
 import syncopy as spy
@@ -28,6 +27,14 @@ class TestFastICA():
         cfg.method = 'fastica'
         res = spy.runica(self.adata, cfg)
         assert type(res) == spy.AnalogData
+
+    def test_ica_fails_with_wrong_input(self):
+        cfg = get_defaults(spy.runica)
+        cfg.method = 'fastica'
+        wrong_data = spy.CrossSpectralData(data=np.ones((2, 2, 2, 2)), samplerate=1)
+        with pytest.raises(SPYTypeError, match='Syncopy AnalogData object'):
+            res = spy.runica(wrong_data, cfg)
+
 
 if __name__ == '__main__':
     T1 = TestFastICA()
